077e5f26765f32aec46432322f5990be7ca8c4d5
Robert Thompson <robertdavidthompson632@gmail.com>
2025-04-26 17:51:12 -0400

    removing the old apps to regenerate as remote

--- apps/dashboard/src/app/remote-entry/entry.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-@Component({
-  imports: [RouterModule],
-  selector: 'app-dashboard-entry',
-  template: `<router-outlet></router-outlet>`
-})
-export class RemoteEntryComponent {}
--- apps/dashboard/src/app/remote-entry/entry.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-
-export const remoteRoutes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent
-  }
-];
